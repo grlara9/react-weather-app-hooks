c484dfdb7736a39b4c14cdbc72f4a4bc0f423135
Guillermo Lara <grlara9@gmail.com>
2020-10-15 15:15:52 -0700

    get user location and display it as default temperature

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -5,36 +5,37 @@ import axios from 'axios'
     const key = 'ce06c3f81e1990453b38833c502026cb';
     const [location, setLocation]=useState('')
     const [data, setData]=useState({})
-    const [coord, setCoord]=useState({lat:'50', lon:'60'})
+    
 
     useEffect(()=> {
+       
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(position =>{
+              console.log("this is the position" + position.coords.latitude + position.coords.longitude)
               
-              var loc = {
-                lat: position.coords.latitude,
-                lon: position.coords.longitude
-              }
+                  
               
-                setCoord(loc)  
-              
-              const api =`https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&appid=${key}`;
+              const api =`https://api.openweathermap.org/data/2.5/weather?lat=${ position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}`;
               axios.get(api)
               .then(response => {
                 console.log(response)
                 let weather ={
                   city: response.data.name,
                   country:response.data.sys.country,
-                  temperature: this.covertToFahrenheit(response.data.main.temp),
-                  max: this.covertToFahrenheit(response.data.main.temp_max),
-                  min:this.covertToFahrenheit(response.data.main.temp_min),
+                  temperature: response.data.main.temp,
+                  max: response.data.main.temp_max,
+                  min:response.data.main.temp_min,
                   condition: response.data.weather[0].description
                   }
                   setData(weather)
+                
+            
               })
+              .then(err => console.log(err))
+            })
+        }
                 },[])
-            }
-        })
+        
                 return (
                 <div>
 
