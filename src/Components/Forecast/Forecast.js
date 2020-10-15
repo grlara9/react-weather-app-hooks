87e6308bc82c8bae5c113f3e87aac04856e5c0a5
Guillermo Lara <grlara9@gmail.com>
2020-10-15 16:47:23 -0700

    finally can update city and display it

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -5,40 +5,31 @@ import axios from 'axios'
     const key = 'ce06c3f81e1990453b38833c502026cb';
     const [location, setLocation]=useState('')
     const [data, setData]=useState({})
+    const[city, setCity]=useState('')
     
-
     useEffect(()=> {
-       
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(position =>{
               console.log("this is the position" + position.coords.latitude + position.coords.longitude)
               
-                  
-              
-              const api =`https://api.openweathermap.org/data/2.5/weather?lat=${ position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}`;
-              axios.get(api)
-              .then(response => {
+                const api =`https://api.openweathermap.org/data/2.5/weather?lat=${ position.coords.latitude}&lon=${position.coords.longitude}&appid=${key}`;
+                axios.get(api)
+                .then(response => {
                 console.log(response)
-                let weather ={
-                  city: response.data.name,
-                  country:response.data.sys.country,
-                  temperature: response.data.main.temp,
-                  max: response.data.main.temp_max,
-                  min:response.data.main.temp_min,
-                  condition: response.data.weather[0].description
-                  }
-                  setData(weather)
                 
-            
-              })
-              .then(err => console.log(err))
+                  setCity(response.data.name)
+                  
+                  
+                 
+                })
+                .then(err => console.log(err))
             })
         }
-                },[])
+    },[])
         
                 return (
                 <div>
-
+                    {city}
                 </div>
             )
         }
