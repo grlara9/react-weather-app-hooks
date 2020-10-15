7e8dcfd6f146c15040794999a292a6160ec46494
Guillermo Lara <grlara9@gmail.com>
2020-10-14 18:27:42 -0700

    set some constants and started using usestate hook

---
+++ src/Components/Forecast/Forecast.js
@@ -0,0 +1,17 @@
+import React ,{useState, useEffect } from 'react'
+
+ const Forecast = ()=>{
+    const key = 'APP-KEY';
+    const [location, setLocation]=useState('')
+    const [data, setData]=useState({})
+    const [coord, setCoord]=useState({lat:'50', lon:'60'})
+
+    useEffect(()=> {
+
+    },[])
+    return (
+        
+    )
+ }
+ export default Forecast
+
