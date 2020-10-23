6b2aca36b1b6231d998cc29c41b9d2b4fde5dab0
Guillermo Lara <grlara9@gmail.com>
2020-10-22 20:09:23 -0700

    Updates

---
+++ precache-manifest.cf1af32273ce096ed95365a1bdffe2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6258dca37d5ba6f6d5dc9e9b4de39e97",
+    "url": "/react-weather-app-hooks/index.html"
+  },
+  {
+    "revision": "9a1b42563a6997a1384c",
+    "url": "/react-weather-app-hooks/static/css/main.d5598b2c.chunk.css"
+  },
+  {
+    "revision": "02af0d8dbde94d322503",
+    "url": "/react-weather-app-hooks/static/js/2.b3403e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app-hooks/static/js/2.b3403e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1b42563a6997a1384c",
+    "url": "/react-weather-app-hooks/static/js/main.2334d0ec.chunk.js"
+  },
+  {
+    "revision": "9ea45f52c2edd8d8b9a2",
+    "url": "/react-weather-app-hooks/static/js/runtime-main.f9d3f46f.js"
+  }
+]);
