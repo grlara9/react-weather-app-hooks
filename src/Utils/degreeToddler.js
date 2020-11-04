957cac87a0a0c2f1605a378df45286e6f0072897
Guillermo Lara <grlara9@gmail.com>
2020-11-03 19:42:06 -0800

    created form with type radio to choose celsius and fahrenheit

--- src/Utils/degreeToddler.js
+++ src/Utils/degreeToddler.js
@@ -1,5 +1,29 @@
 import React from 'react'
 
-const Toggler =()=>{}
-
+const Toggler =()=>{
+return (
+    <React.Fragment>
+        <div className="form-check form-check-inline">
+            <input
+            class="form-check-input"
+            type="radio"
+            name="degree-type"
+            id="celsius"
+            value="metric"
+            />
+            <label class="form-check-label" for="celsius">Celsius</label>
+        </div>
+        <div className="form-check form-check-inline">
+            <input
+            class="form-check-input"
+            type="radio"
+            name="degree-type"
+            id="farenheit"
+            value="imperial"
+            />
+            <label class="form-check-label" for="farenheit">Farenheit</label>
+        </div>
+    </React.Fragment>
+  )
+}
 export default Toggler
