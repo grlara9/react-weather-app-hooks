cb4c9f9558278ed91f1ab1a51f10f2fa9b56ba82
Guillermo Lara <grlara9@gmail.com>
2021-02-20 22:56:20 -0800

    used another api to obtain forecast for the next few days

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -8,8 +8,10 @@ import DisplayWeather from '../../Utils/displayWeather'
 import { convertToFahrenheit } from '../../Utils/functions';
 
 const Forecast = ()=>{
-   const KEY_API='ce06c3f81e1990453b38833c502026cb';
-    
+   //const KEY_API='ce06c3f81e1990453b38833c502026cb';
+   const KEY_API="fd65dfc39c19c48cf92e9b8454d0686d";
+ 
+
     const [inputText, setInputText] = useState('')
     const [data, setData]= useState({})
    const [degreeType, setDegreeType]=useState('fahrenheit')
@@ -19,23 +21,26 @@ const Forecast = ()=>{
             navigator.geolocation.getCurrentPosition(position =>{
               console.log("this is the position" + position.coords.latitude + position.coords.longitude)
               
-                const api =`https://api.openweathermap.org/data/2.5/weather?lat=${ position.coords.latitude}&lon=${position.coords.longitude}&appid=${KEY_API}`;
+                //const api =`https://api.openweathermap.org/data/2.5/weather?lat=${ position.coords.latitude}&lon=${position.coords.longitude}&appid=${KEY_API}`;
+                const api =`http://api.openweathermap.org/data/2.5/forecast?lat=${ position.coords.latitude}&lon=${position.coords.longitude}&appid=${KEY_API}`
                 axios.get(api)
-                .then(response => 
-                setData({
-                    city: response.data.name,
-                    country: response.data.sys.country,
-                    temperature: response.data.main.temp,
-                    max: response.data.main.temp_max,
-                    min: response.data.main.temp_min,
-                    condition: response.data.weather[0].description,
-                    icon: response.data.weather[0].icon
-               
+                .then(response =>{ 
+                console.log(response)
+                    setData({
+                        city: response.data.city.name,
+                        country:response.data.city.country,
+                        temperature:  response.data.list[0].main.temp,
+                        max: response.data.list[0].main.temp_max,
+                        min: response.data.list[0].main.temp_min,
+                        condition: response.data.list[0].description,
+                        icon: response.data.list[0].weather[0].icon
+                    })
+                }
+                    )
+                    .then(err => console.log(err))
                 })
-                )
-                .then(err => console.log(err))
-            })
-        }
+               
+            }
     },[])
    
    const getWeather = (e)=>{
