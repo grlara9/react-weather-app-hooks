88396fb39fbb37bd9cda78f743337d5f63a85f77
Guillermo Lara <grlara9@gmail.com>
2021-02-24 23:57:33 -0800

    added some style to the card

--- src/Utils/displayForecast.js
+++ src/Utils/displayForecast.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
+
 import './displayForecast.css'
 
 const DisplayForecast = ({dt, temp_min, temp_max, main, icon}) => {
@@ -7,14 +7,18 @@ const DisplayForecast = ({dt, temp_min, temp_max, main, icon}) => {
   // create a date object with Date class constructor
   const date = new Date(dt);
   return (
+   <div className="card-container">
+
     <div className="card">
-      <div className="card-img"
+      <img className="card-img"
         variant="top"
         // get the src from example url and pass the icon prop for icon code
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
       />
       <div className="card-body">
-        <Card.Title>{main}</Card.Title>
+        <div className="card-title">
+          {main}
+        </div>
         {/*  datetime is received in milliseconds, let's turn into local date time */}
         <p>
           {date.toLocaleDateString()} - {date.toLocaleTimeString()}
@@ -25,6 +29,7 @@ const DisplayForecast = ({dt, temp_min, temp_max, main, icon}) => {
         <p>Max: {temp_max}</p>
       </div>
     </div>
+    </div> 
   );
 };
 export default DisplayForecast;
