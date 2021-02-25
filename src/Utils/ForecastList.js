817800742e3f2328c79e394093c2334df4be2e68
Guillermo Lara <grlara9@gmail.com>
2021-02-24 23:37:50 -0800

    work on structure of the card that will display forecast fo next couple of days

--- src/Utils/ForecastList.js
+++ src/Utils/ForecastList.js
@@ -9,10 +9,15 @@ const ForecastList = ({weathers}) => {
            {weathers 
            ? weathers.map(({dt,main, weather}) => (
               <div key={dt}>
-                  <DisplayForecast temp_max={main.temp_max} temp_min={main.temp_min} dt={dt * 1000} main={weather[0].main} icon={weather[0].icon}/>
+                  <DisplayForecast 
+                        temp_max={main.temp_max} 
+                        temp_min={main.temp_min} 
+                        dt={dt * 1000} 
+                        main={weather[0].main} 
+                        icon={weather[0].icon}/>
                 </div>
             )
-            ):"nothing"}
+            ):"not working"}
         </div>
     )
 }
--- src/Utils/displayForecast.js
+++ src/Utils/displayForecast.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Card} from 'react-bootstrap';
+import './displayForecast.css'
 
 const DisplayForecast = ({dt, temp_min, temp_max, main, icon}) => {
    
   // create a date object with Date class constructor
   const date = new Date(dt);
   return (
-    <Card>
-      <Card.Img
+    <div className="card">
+      <div className="card-img"
         variant="top"
         // get the src from example url and pass the icon prop for icon code
         src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
       />
-      <Card.Body>
+      <div className="card-body">
         <Card.Title>{main}</Card.Title>
         {/*  datetime is received in milliseconds, let's turn into local date time */}
         <p>
@@ -22,8 +23,8 @@ const DisplayForecast = ({dt, temp_min, temp_max, main, icon}) => {
         <p>Min: {temp_min}</p>
         {/* maximum temperature */}
         <p>Max: {temp_max}</p>
-      </Card.Body>
-    </Card>
+      </div>
+    </div>
   );
 };
 export default DisplayForecast;
